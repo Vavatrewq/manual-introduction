e094818e40d34e78882e55f98cfd19a0fb08cec0
Vavatrewq <wagnerw651@gmail.com>
2023-03-22 11:47:43 -0300

    home-responsive/manual

--- src/components/homes/Home.tsx
+++ src/components/homes/Home.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import icon_book from 'src/assets/icons/icon_book.png'
 import icon_fap from 'src/assets/icons/fap-logo.png'
+import { FcGraduationCap } from 'react-icons/fc'
 import { StartLevel, SecondLevel, ThirdLevel } from '../sections/InfoPage'
 import './leave.css'
+import './leaveResponsive.css'
 
 export function Home(): JSX.Element {
   const [infoPageStart, setInfopageStart] = useState(false)
@@ -17,35 +19,6 @@ export function Home(): JSX.Element {
 
   return (
     <>
-      <section className="body-right">
-        <div className="show-content">
-          <img className="logo_fap space-top" src={icon_fap} alt="logo_fap" />
-
-          <p className="show-text space-top">
-            Esse projeto foi pensado para cumprir com dever de ensinar pessoas
-            com dificuldades em informática básica. Portanto nos alunos da FAP
-            oferecemos um manual básico, e selecionamos níveis de experiencia
-            para cada um.
-          </p>
-
-          <div className="show-btn space-top">
-            <button onClick={() => setInfopageStart(true)}>1exemplo</button>
-            <button onClick={() => setInfopageSecond(true)}>2exemplo</button>
-            <button onClick={() => setInfopageThird(true)}>exemplo</button>
-          </div>
-
-          <div className="space-top">
-            <ul className="student-authors">
-              <li>Name: Student</li>
-              <li>Name: Student</li>
-              <li>Name: Student</li>
-              <li>Name: Student</li>
-              <li>Name: Student</li>
-            </ul>
-          </div>
-        </div>
-      </section>
-
       <section className="body-left">
         <div className="element-block">
           <span className="block-page"></span>
@@ -59,8 +32,82 @@ export function Home(): JSX.Element {
             <span className="circle-page"></span>
           </div>
 
-          <h1 className="title-text">Informática Básica Online - Manual</h1>
+          <img
+            className="logo_fap-top space-top"
+            src={icon_fap}
+            alt="logo_fap"
+          />
+
           <img className="img_book" src={icon_book} alt="img_book" />
+          <h1 className="title-text">Informática Básica Online - Manual</h1>
+        </div>
+        <p className="show-text-start">
+          Esse projeto foi pensado para cumprir com dever de ensinar pessoas com
+          dificuldades em informática básica. Portanto nos alunos da FAP
+          oferecemos um manual básico, e selecionamos níveis de experiencia para
+          cada um.
+        </p>
+
+        <div className="element-block-botton">
+          <span className="block-page"></span>
+        </div>
+      </section>
+
+      <section className="body-right">
+        <div className="show-content">
+          <img className="logo_fap space-top" src={icon_fap} alt="logo_fap" />
+
+          <p className="show-text space-top">
+            Esse projeto foi pensado para cumprir com dever de ensinar pessoas
+            com dificuldades em informática básica. Portanto nos alunos da FAP
+            oferecemos um manual básico, e selecionamos níveis de experiencia
+            para cada um.
+          </p>
+
+          <div className="show-btn space-top">
+            <p className="box-btn">
+              <button
+                className="btn-start btn-circle"
+                onClick={() => setInfopageStart(true)}
+              ></button>
+              Iniciante
+            </p>
+            <p className="box-btn">
+              <button
+                className="btn-between btn-circle"
+                onClick={() => setInfopageSecond(true)}
+              ></button>
+              Intermediário
+            </p>
+            <p className="box-btn">
+              <button
+                className="btn-third btn-circle"
+                onClick={() => setInfopageThird(true)}
+              ></button>
+              Avançado
+            </p>
+          </div>
+
+          <div className="space-top">
+            <h2>Criadores:</h2>
+            <ul className="student-authors">
+              <li>
+                <FcGraduationCap /> Name: Student
+              </li>
+              <li>
+                <FcGraduationCap /> Name: Student
+              </li>
+              <li>
+                <FcGraduationCap /> Name: Student
+              </li>
+              <li>
+                <FcGraduationCap /> Name: Student
+              </li>
+              <li>
+                <FcGraduationCap /> Name: Student
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     </>
